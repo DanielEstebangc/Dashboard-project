66ada7168dd1a696082abe49fbeb1cdc875dcac7
Daniel Galvis <daniel.galvis@dentsu.com>
2025-11-05 16:04:03 -0500

    se quitaron importaciones que no se usan de paginaslista y al grafico funnel se le agrego la primera coneccion de la seccion Awareness con la pagina Awareness

--- client/src/components/charts/charts_executive/chartfunnel/Funnelchart.jsx
+++ client/src/components/charts/charts_executive/chartfunnel/Funnelchart.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { useNavigate } from "react-router-dom";
+
 import {
   FunnelChart,
   Funnel,
@@ -9,7 +10,7 @@ import {
 } from "recharts";
 
 const data = [
-  { name: "Gasto Awareness", value: 277590326, fill: "#9E2D38" },
+  { name: "Gasto Awareness", value: 277590326, fill: "#9E2D38",link:"/awareness-performance" },
   { name: "Consideration", value: 277979326, fill: "#666666" },
   { name: "Purchase", value: 277306326, fill: "#2B5A7A" },
 ];
@@ -34,6 +35,7 @@ const CustomTooltip = ({ active, payload }) => {
 };
 
 const CustomFunnelChart = () => {
+  const navigate = useNavigate();
   return (
     <div className="chart-no-outline flex flex-col justify-center items-centerblock box-border w-[373.406px] h-[300px] min-w-0 text-[oklch(0.145_0_0)] text-[14px] leading-[21px] text-left font-sans font-normal m-0 p-0 border-[rgba(133, 60, 60, 0.8)] border-t-0 border-r-0 border-b-0 border-l-0 outline-[oklab(0.708_0_0_/_0.5)] [tab-size:4] [unicode-bidi:isolate] [text-size-adjust:100%] [border-image:unset] [border-image-outset:0] [border-image-repeat:stretch] [border-image-slice:100%] [border-image-width:1] [--tw-border-opacity:1]">
       <div className="chart-no-outline flex flex-col justify-center items-centerw-[330px] h-[300px] p-4">
@@ -48,9 +50,9 @@ const CustomFunnelChart = () => {
               stroke="#fff"
             >
               {data.map((entry, index) => (
-                <Cell key={`cell-${index}`} fill={entry.fill} />
+                <Cell key={`cell-${index}`} fill={entry.fill}  style={{ cursor: "pointer" }}  onClick={() => navigate(entry.link)}/>
               ))}
-              <LabelList position="center" dataKey="name" />
+              <LabelList position="center" dataKey="name"  />
             </Funnel>
           </FunnelChart>
         </ResponsiveContainer>
