e4affb62f4f14eec769f35a2e5e6b13f8082a544
Daniel Galvis <danielgalvis.3107@gmail.com>
2026-01-30 10:09:02 -0500

    se quita el server backend js

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
-import express from "express";
-import cors from "cors";
-import chartRoutes from "./src/routes/chart.routes.js";
+// import express from "express";
+// import cors from "cors";
+// import chartRoutes from "./src/routes/chart.routes.js";
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
 
-// Rutas
-app.use("/api/chart", chartRoutes);
+// // Rutas
+// app.use("/api/chart", chartRoutes);
 
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log("Server running on port", PORT));
+// const PORT = process.env.PORT || 4000;
+// app.listen(PORT, () => console.log("Server running on port", PORT));
